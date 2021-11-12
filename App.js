5c4232683170f173b44911868e82f7a7150d3c88
mamencode <mamencode@gmail.com>
2021-11-12 22:00:28 +0300

    expo from local

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import tw from "tailwind-rn"
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={tw('flex-1 bg-red-500 justify-center')}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
